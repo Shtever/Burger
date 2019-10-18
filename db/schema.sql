a4ff8c6b53abca05ada54e11d2401f97ba125c7a
Stephen McMillen <Shtevetm@gmail.com>
2019-10-17 21:47:33 -0500

    Code functional after HEAVY referencing of Sleepy Cats in-class exercise

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+)
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Double-Double', false);
+INSERT INTO burgers (name, devoured) VALUES ('Jalepeño Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Rabbit Burger', false);
